83768b4304ecc400b190c7492184f7a2acfbc23d
John Doe <johndoe@example.com>
2026-05-20 16:19:38 +0200

    fixed spelling mistake in button

--- JS/main.js
+++ JS/main.js
@@ -1,74 +1,70 @@
-
 let niewiem = document.getElementById("changefont");
 let fontChange = document.getElementById("font");
-fontChange.addEventListener('click', open);
-let change = document.getElementById("not")
-change.addEventListener('click', differFont)
+fontChange.addEventListener("click", open);
+let change = document.getElementById("not");
+change.addEventListener("click", differFont);
 
 function attachEvents() {
-    let fontChange = document.getElementById("font");
-    let change = document.getElementById("not")
-    fontChange.addEventListener('click', open);
-    change.addEventListener('click', differFont)
+  let fontChange = document.getElementById("font");
+  let change = document.getElementById("not");
+  fontChange.addEventListener("click", open);
+  change.addEventListener("click", differFont);
 }
 
-
 function open() {
-    if (niewiem.classList.contains("open")) {
-        niewiem.classList.toggle("open");
-        niewiem.innerHTML = `
+  if (niewiem.classList.contains("open")) {
+    niewiem.classList.toggle("open");
+    niewiem.innerHTML = `
         <button id="font">Aa</button>
         `;
-        attachEvents();
-    }
-    else {
-        niewiem.classList.toggle("open");
-        niewiem.innerHTML = `
+    attachEvents();
+  } else {
+    niewiem.classList.toggle("open");
+    niewiem.innerHTML = `
         <button id="font">Aa</button>
         <div class="deafult">
-            <button id="active">Deafault</button>
+            <button id="active">Default</button>
             <button id="not">Readable</button>
         </div>`;
-        attachEvents();
-    }
+    attachEvents();
+  }
 }
 
 function differFont() {
-    let change = document.getElementById("not")
-    if (change.innerHTML == `Readable`) {
-        niewiem.innerHTML = `
+  let change = document.getElementById("not");
+  if (change.innerHTML == `Readable`) {
+    niewiem.innerHTML = `
         <button id="font">Aa</button>
         <div class="deafult">
-            <button id="not">Deafault</button>
+            <button id="not">Default</button>
             <button id="active">Readable</button>
         </div>`;
-        attachEvents();
-        makeFont();
-    }
-    else {
-        niewiem.innerHTML = `
+    attachEvents();
+    makeFont();
+  } else {
+    niewiem.innerHTML = `
         <button id="font">Aa</button>
         <div class="deafult">
-            <button id="active">Deafault</button>
+            <button id="active">Default</button>
             <button id="not">Readable</button>
         </div>`;
-        attachEvents();
-        takeFont();
-    }
+    attachEvents();
+    takeFont();
+  }
 }
 
 function makeFont() {
-    let headings = document.querySelectorAll("h1");
+  let headings = document.querySelectorAll("h1");
 
-    headings.forEach((heading) => {
-        heading.classList.add("readable");
-    });
+  headings.forEach((heading) => {
+    heading.classList.add("readable");
+  });
 }
 
 function takeFont() {
-    let headings = document.querySelectorAll("h1");
+  let headings = document.querySelectorAll("h1");
 
-    headings.forEach((heading) => {
-        heading.classList.remove("readable");
-    });
+  headings.forEach((heading) => {
+    heading.classList.remove("readable");
+  });
 }
